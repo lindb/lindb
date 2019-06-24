6f2ee8c2f9262d54cf50b3e1451e363774223ac5
chunle.pcl <chunle.pcl@alibaba-inc.com>
2019-06-24 17:39:17 +0800

    [feat:eleme#13 #29][broker:rest]: add rest api test and modify comments

--- service/database.go
+++ service/database.go
@@ -16,7 +16,7 @@ type DatabaseService interface {
 	// Create creates or update database based on config
 	Create(database option.Database) error
 	// Get gets database config by databaseName
-	Get(databaseName string) (*option.Database, error)
+	Get(databaseName string) (option.Database, error)
 }
 
 // databaseService implements DatabaseService interface
@@ -24,7 +24,7 @@ type databaseService struct {
 	repo state.Repository
 }
 
-// New creates database service
+// New creates the global database service
 func New() DatabaseService {
 	return &databaseService{
 		repo: state.GetRepo(),
@@ -40,23 +40,24 @@ func (db *databaseService) Create(database option.Database) error {
 	return db.repo.Put(context.TODO(), getDataBasePath(database.Name), data)
 }
 
-func (db *databaseService) Get(databaseName string) (*option.Database, error) {
+// Get returns the database config in the state's repo
+func (db *databaseService) Get(databaseName string) (option.Database, error) {
+	database := option.Database{}
 	if databaseName == "" {
-		return nil, fmt.Errorf("database name must not be null")
+		return database, fmt.Errorf("database name must not be null")
 	}
 	configBytes, err := db.repo.Get(context.TODO(), getDataBasePath(databaseName))
 	if err != nil {
-		return nil, err
+		return database, err
 	}
-	database := &option.Database{}
-	err = json.Unmarshal(configBytes, database)
+	err = json.Unmarshal(configBytes, &database)
 	if err != nil {
-		return nil, err
+		return database, err
 	}
-
 	return database, nil
 }
 
+// getDataBasePath gets the path where the database config is stored
 func getDataBasePath(databaseName string) string {
 	return databaseConfigNode + "/" + databaseName
 }
