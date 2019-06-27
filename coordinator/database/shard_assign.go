519917bfdc0b6c15fa55443398f76f02fdff2678
stone1100 <stonejh1100@gmail.com>
2019-06-27 17:30:01 +0800

    [feat:eleme#18][coordinator:shard_assign]: shard assigns replica list for database creation

---
+++ coordinator/database/shard_assign.go
@@ -0,0 +1,96 @@
+package database
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/eleme/lindb/pkg/option"
+)
+
+// Shard assigment reference kafka partition assigment
+// kafka implement => (https://github.com/apache/kafka/blob/2.3/core/src/main/scala/kafka/admin/AdminUtils.scala)
+
+// ShardAssignment assigns replica list for database's each shard based on selected node list
+// There are 2 goals of replica assignment:
+// 1. Spread the replicas evenly among storage nodes for currently cluster state.
+// 2. For shards assigned to a particular storage node, their other replicas are spread over the other storage nodes.
+//
+// TO achieve this goal, we:
+// 1. Assign the first replica of each shard by round-robin, starting from a random position in the storage node list.
+// 2. Assign the remaining replicas of each shard with an increasing shift.
+//
+// Here is an example of assigning, (num. of nodes = 5, num of shards = 10, replica factor = 3)
+// node-0	node-1	node-2	node-3	node-4
+// s0		s1		s2		s3		s4		(1st replica)
+// s5		s6		s7		s8		s9		(1st replica)
+// s4		s0		s1		s2		s3		(2st replica)
+// s8		s9		s5		s6		s7		(2st replica)
+// s3		s4		s0		s1		s2		(3st replica)
+// s7		s8		s9		s5		s6		(3st replica)
+func ShardAssignment(storageNodeIDs []int, database option.Database) (*option.ShardAssignment, error) {
+	numOfShard := database.NumOfShard
+	replicaFactor := database.ReplicaFactor
+	if numOfShard <= 0 {
+		return nil, fmt.Errorf("shard assign error for database[%s], because num. of shard <=0", database.Name)
+	}
+	if replicaFactor <= 0 {
+		return nil, fmt.Errorf("shard assign error for database[%s], bacause replica factor <=0", database.Name)
+	}
+	if replicaFactor > len(storageNodeIDs) {
+		return nil,
+			fmt.Errorf("shard assign error for database[%s], bacause replica factor > num. of storage nodes",
+				database.Name)
+	}
+
+	shardAssignment := option.NewShardAssignment()
+	assignReplicasToStorages(storageNodeIDs, numOfShard, replicaFactor, -1, -1, shardAssignment)
+
+	return shardAssignment, nil
+}
+
+// assignReplicasToStorages assigns replica list for database's each shard, return shard assignment result
+func assignReplicasToStorages(storageNodeIDs []int,
+	numOfShard, replicaFactor, fixedStartIndex, startShardID int,
+	shardAssignment *option.ShardAssignment) {
+	numOfNode := len(storageNodeIDs)
+
+	// init start index/shift/current shard
+	startIndex := fixedStartIndex
+	nextReplicaShift := fixedStartIndex
+	if fixedStartIndex < 0 {
+		startIndex = rand.Intn(numOfNode)
+		nextReplicaShift = rand.Intn(numOfNode)
+	}
+	currentShardID := 0
+	if startShardID >= 0 {
+		currentShardID = startShardID
+	}
+
+	// assign replica list for each shard
+	for i := 0; i < numOfShard; i++ {
+		if currentShardID > 0 && (currentShardID%numOfNode == 0) {
+			nextReplicaShift++
+		}
+		firstReplicaIndex := (currentShardID + startIndex) % numOfNode
+
+		// elect first replica as leader
+		leader := storageNodeIDs[firstReplicaIndex]
+		shardAssignment.AddReplica(int32(currentShardID), leader)
+
+		// assign other replica
+		for j := 0; j < replicaFactor-1; j++ {
+			idx := replicaIndex(firstReplicaIndex, nextReplicaShift, j, numOfNode)
+			shardAssignment.AddReplica(int32(currentShardID), storageNodeIDs[idx])
+		}
+
+		// do next shard assign
+		currentShardID++
+	}
+
+}
+
+// replicaIndex calculates replica index based on firist replica index and shift
+func replicaIndex(firstReplicaIndex, secondReplicaShift, replicaIndex, numOfNode int) int {
+	shift := 1 + (secondReplicaShift+replicaIndex)%(numOfNode-1)
+	return (firstReplicaIndex + shift) % numOfNode
+}
---
+++ coordinator/database/shard_assign_test.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/eleme/lindb/pkg/option"
+)
+
+func TestShardAssign(t *testing.T) {
+	storageNodeIDs := []int{0, 1, 2, 3, 4}
+
+	_, err1 := ShardAssignment(storageNodeIDs,
+		option.Database{
+			Name:          "test",
+			NumOfShard:    0,
+			ReplicaFactor: 3,
+		})
+	assert.NotNil(t, err1)
+
+	_, err2 := ShardAssignment(storageNodeIDs,
+		option.Database{
+			Name:          "test",
+			NumOfShard:    10,
+			ReplicaFactor: 6,
+		})
+	assert.NotNil(t, err2)
+
+	shardAssignment, _ := ShardAssignment(storageNodeIDs,
+		option.Database{
+			Name:          "test",
+			NumOfShard:    10,
+			ReplicaFactor: 3,
+		})
+
+	shardAssignment2 := option.NewShardAssignment()
+	shardAssignment2.AddReplica(0, 0)
+	shardAssignment2.AddReplica(0, 1)
+	shardAssignment2.AddReplica(0, 2)
+
+	shardAssignment2.AddReplica(1, 1)
+	shardAssignment2.AddReplica(1, 2)
+	shardAssignment2.AddReplica(1, 3)
+
+	shardAssignment2.AddReplica(2, 2)
+	shardAssignment2.AddReplica(2, 3)
+	shardAssignment2.AddReplica(2, 4)
+
+	shardAssignment2.AddReplica(3, 3)
+	shardAssignment2.AddReplica(3, 4)
+	shardAssignment2.AddReplica(3, 0)
+
+	shardAssignment2.AddReplica(4, 4)
+	shardAssignment2.AddReplica(4, 0)
+	shardAssignment2.AddReplica(4, 1)
+
+	shardAssignment2.AddReplica(5, 0)
+	shardAssignment2.AddReplica(5, 2)
+	shardAssignment2.AddReplica(5, 3)
+
+	shardAssignment2.AddReplica(6, 1)
+	shardAssignment2.AddReplica(6, 3)
+	shardAssignment2.AddReplica(6, 4)
+
+	shardAssignment2.AddReplica(7, 2)
+	shardAssignment2.AddReplica(7, 4)
+	shardAssignment2.AddReplica(7, 0)
+
+	shardAssignment2.AddReplica(8, 3)
+	shardAssignment2.AddReplica(8, 0)
+	shardAssignment2.AddReplica(8, 1)
+
+	shardAssignment2.AddReplica(9, 4)
+	shardAssignment2.AddReplica(9, 1)
+	shardAssignment2.AddReplica(9, 2)
+
+	assert.Equal(t, *shardAssignment, *shardAssignment2)
+}
