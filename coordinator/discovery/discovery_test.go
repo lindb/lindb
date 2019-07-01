23d6212134e17dfe52b39f66afbad66bb7e400a7
blacksky <peichunle@163.com>
2019-07-01 17:25:17 +0800

    [feat:eleme#36][coordinator:elect]: master elect based on etcd

---
+++ coordinator/discovery/discovery_test.go
@@ -0,0 +1,45 @@
+package discovery
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/eleme/lindb/pkg/state"
+
+	etcd "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/integration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNodeList(t *testing.T) {
+	clus := integration.NewClusterV3(t, &integration.ClusterConfig{Size: 1})
+	defer clus.Terminate(t)
+
+	_ = state.New("etcd", etcd.Config{
+		Endpoints: []string{clus.Members[0].GRPCAddr()},
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	newDiscovery, err := NewDiscovery(ctx, "/test/")
+
+	if err != nil {
+		t.Errorf("Discovery error :%s", err.Error())
+	}
+	if newDiscovery == nil {
+		t.Error("Discovery is empty")
+		return
+	}
+	nodeList := newDiscovery.NodeList()
+	assert.Equal(t, 0, len(nodeList))
+
+	repo := state.GetRepo()
+	node := Node{"127.0.0.1", 2080}
+	bytes, _ := json.Marshal(node)
+	_ = repo.Put(context.TODO(), "/test/key1", bytes)
+	_ = repo.Put(context.TODO(), "/test/key2", bytes)
+	_ = repo.Put(context.TODO(), "/test/key3", bytes)
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, 3, len(newDiscovery.NodeList()))
+}
--- pkg/state/etcd_test.go
+++ pkg/state/etcd_test.go
@@ -9,11 +9,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/eleme/lindb/pkg/util"
+
 	etcd "github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/integration"
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/check.v1"
-
-	"github.com/eleme/lindb/pkg/util"
 )
 
 type address struct {
@@ -67,15 +68,16 @@ func (ts *testEtcdRepoSuite) Test_Write_Read(c *check.C) {
 		c.Fatal(err1)
 	}
 	home2 := &address{}
-	json.Unmarshal(d1, home2)
+	_ = json.Unmarshal(d1, home2)
 	c.Assert(*home1, check.Equals, *home2)
 
-	rep.Delete(context.TODO(), "/test/key1")
+	_ = rep.Delete(context.TODO(), "/test/key1")
 
 	_, err2 := rep.Get(context.TODO(), "/test/key1")
 	c.Assert(err2, check.NotNil)
 
-	rep.Close()
+	_ = rep.Close()
+
 }
 
 func (ts *testEtcdRepoSuite) TestNew(c *check.C) {
@@ -135,10 +137,10 @@ func (ts *testEtcdRepoSuite) TestWatch(c *check.C) {
 			mutex.Unlock()
 		}
 	}()
-	b.Put(ctx, "/cluster1/controller/1", []byte("1"))
+	_ = b.Put(ctx, "/cluster1/controller/1", []byte("1"))
 
 	// test watch exist path
-	b.Put(ctx, "/cluster1/controller/2", []byte("2"))
+	_ = b.Put(ctx, "/cluster1/controller/2", []byte("2"))
 	ch2, err2 := b.Watch(ctx, "/cluster1/controller/2")
 	if err2 != nil {
 		c.Fatal(err2)
@@ -152,7 +154,7 @@ func (ts *testEtcdRepoSuite) TestWatch(c *check.C) {
 		}
 	}()
 	// modify value of key2
-	b.Put(ctx, "/cluster1/controller/2", []byte("222"))
+	_ = b.Put(ctx, "/cluster1/controller/2", []byte("222"))
 	time.Sleep(100 * time.Millisecond)
 	cancel()
 
@@ -164,4 +166,101 @@ func (ts *testEtcdRepoSuite) TestWatch(c *check.C) {
 	c.Assert(val["/cluster1/controller/1"], check.Equals, "1")
 	c.Assert(val["/cluster1/controller/2"], check.Equals, "222")
 	mutex.RUnlock()
+}
+
+func (ts *testEtcdRepoSuite) TestGetWatchPrefix(c *check.C) {
+	b, _ := newEtedRepository(cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	_ = b.Put(context.TODO(), "/lindb/broker/1", []byte("1"))
+
+	_ = b.Put(context.TODO(), "/lindb/broker/2", []byte("2"))
+
+	ch, err := b.Watch(ctx, "/lindb/broker")
+	if err != nil {
+		c.Fatal(err)
+	}
+
+	go func() {
+		for event := range ch {
+			println(event.Key + string(event.Value))
+			c.Assert(event.Key, check.Equals, "/lindb/broker/3")
+			c.Assert(string(event.Value), check.Equals, "3")
+			switch event.Type {
+			case EventTypeModify:
+
+			case EventTypeDelete:
+			}
+		}
+	}()
+
+	_ = b.Put(context.TODO(), "/lindb/broker/3", []byte("3"))
+	bytes1, _ := b.Get(context.TODO(), "/lindb/broker/3")
+	c.Assert(string(bytes1), check.Equals, "3")
+	time.Sleep(10 * time.Second)
+	cancel()
+}
+
+func (ts *testEtcdRepoSuite) TestPutIfNotExitAndKeepLease(c *check.C) {
+	b, _ := newEtedRepository(cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+	// the key should not exist,it must be success
+	success, ch, err := b.PutIfNotExist(ctx, "/lindb/breoker/master", []byte("test"), 1)
+	if err != nil {
+		c.Fatal(err)
+	}
+	c.Assert(success, check.Equals, true)
+	time.Sleep(2 * time.Second)
+	bytes, err := b.Get(context.TODO(), "/lindb/breoker/master")
+	if err != nil {
+		c.Fatal(err)
+	}
+	c.Assert(string(bytes), check.Equals, "test")
+
+	ctx2, cancel2 := context.WithCancel(context.Background())
+	shouldFalse, _, _ := b.PutIfNotExist(ctx2, "/lindb/breoker/master", []byte("test2"), 1)
+	if cancel2 != nil {
+		cancel2()
+	}
+	c.Assert(shouldFalse, check.Equals, false)
+	cancel()
+	select {
+	case <-ch:
+	case <-time.After(500 * time.Millisecond):
+		c.Fatal("cancel heartbeat timeout")
+	}
+
+	time.Sleep(2 * time.Second)
+
+	_, err = b.Get(context.TODO(), "/lindb/breoker/master")
+	if err == nil {
+		c.Fatal("the key should not exist")
+	}
+
+	ctx3, cancel3 := context.WithCancel(context.Background())
+	shouldSuccess, _, _ := b.PutIfNotExist(ctx3, "/lindb/breoker/master", []byte("test3"), 1)
+	c.Assert(shouldSuccess, check.Equals, true)
+
+	bytes3, _ := b.Get(context.TODO(), "/lindb/breoker/master")
+
+	c.Assert(string(bytes3), check.Equals, "test3")
+
+	cancel3()
+}
+func (ts *testEtcdRepoSuite) TestDeleteWithTheValue(c *check.C) {
+	b, _ := newEtedRepository(cfg)
+	_ = b.Put(context.TODO(), "test", []byte("value1"))
+	success, err := b.DeleteWithValue(context.TODO(), "test", []byte("value1"))
+	if err != nil {
+		c.Fatal("the operation should be success")
+	}
+	assert.True(c, success)
+
+	_ = b.Put(context.TODO(), "test", []byte("value2"))
+	success2, err := b.DeleteWithValue(context.TODO(), "test", []byte("value1"))
+	if err != nil {
+		c.Fatal("the operation should not have error")
+	}
+	assert.False(c, success2)
+
 }
