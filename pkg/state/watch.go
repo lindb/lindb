cf783cfa97eeb84d477622b0734e6fa41ff97e82
XiaoChao Dong (@damnever) <dxc.wolf@gmail.com>
2019-07-05 17:27:26 +0800

    [feat:#23][pkg:etcd-cli]: make watch related API retry-able

---
+++ pkg/state/watch.go
@@ -0,0 +1,113 @@
+package state
+
+import (
+	"context"
+	"time"
+
+	etcdcliv3 "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+)
+
+const (
+	defaultRetryInterval = 100 * time.Millisecond
+)
+
+type watcher struct {
+	ctx  context.Context
+	cli  *etcdRepository
+	key  string
+	opts []etcdcliv3.OpOption
+
+	EventC WatchEventChan
+}
+
+func newWatcher(ctx context.Context, cli *etcdRepository, key string, opts ...etcdcliv3.OpOption) *watcher {
+	eventc := make(chan *Event)
+	w := &watcher{
+		ctx:  ctx,
+		cli:  cli,
+		key:  key,
+		opts: opts,
+
+		EventC: eventc,
+	}
+	go w.watch(eventc)
+	return w
+}
+
+func (w *watcher) watch(eventc chan<- *Event) {
+	defer close(eventc)
+
+	cli := w.cli.client
+	var evtAll *Event
+	var resp *etcdcliv3.GetResponse
+	// The etcdcliv3.Watch may fail if ErrCompacted or other errors occurs.
+	for {
+		for {
+			var err error
+			if resp, err = cli.Get(w.ctx, w.key, w.opts...); err == nil {
+				evtAll = packAllEvents(resp.Kvs)
+				break
+			}
+			select {
+			case <-w.ctx.Done():
+				return
+			case <-time.After(defaultRetryInterval):
+			}
+		}
+		select {
+		case <-w.ctx.Done():
+			return
+		case eventc <- evtAll:
+		}
+
+		opts := append(w.opts, etcdcliv3.WithRev(resp.Header.Revision+1))
+		wchc := cli.Watch(w.ctx, w.key, opts...)
+		if wchc == nil {
+			continue
+		}
+		for watchResp := range wchc {
+			if err := watchResp.Err(); err != nil {
+				select {
+				case <-w.ctx.Done():
+					return
+				case eventc <- &Event{Err: err}:
+				}
+				continue
+			}
+			for _, event := range watchResp.Events {
+				select {
+				case <-w.ctx.Done():
+					return
+				case eventc <- packWatchEvent(event):
+				}
+			}
+		}
+	}
+}
+
+func packWatchEvent(watchEvent *etcdcliv3.Event) *Event {
+	kv := watchEvent.Kv
+	evt := &Event{
+		Type: EventTypeModify,
+		KeyValues: []EventKeyValue{
+			{Key: string(kv.Key), Value: kv.Value, Rev: kv.ModRevision},
+		},
+	}
+	if watchEvent.Type == mvccpb.DELETE {
+		evt.Type = EventTypeDelete
+	}
+	return evt
+}
+
+func packAllEvents(kvs []*mvccpb.KeyValue) *Event {
+	evt := &Event{Type: EventTypeAll}
+	for _, kv := range kvs {
+		evt.KeyValues = append(evt.KeyValues, EventKeyValue{
+			Key:   string(kv.Key),
+			Value: kv.Value,
+			Rev:   kv.ModRevision,
+		})
+	}
+	return evt
+}
