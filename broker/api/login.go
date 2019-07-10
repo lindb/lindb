e076788e490f4204afbc3c52b2a2e6807c6a69c2
dupeng1942 <dp177971@alibaba-inc.com>
2019-07-10 15:28:16 +0800

    [feat:#50][api:auth]:auth for admin rest api

---
+++ broker/api/login.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/eleme/lindb/broker/middleware"
+	"github.com/eleme/lindb/models"
+)
+
+// LoginAPI represents login param
+type LoginAPI struct {
+	user models.User
+}
+
+// NewLoginAPI creates login api instance
+func NewLoginAPI(user models.User) *LoginAPI {
+	return &LoginAPI{
+		user: user,
+	}
+}
+
+// Login responses unique token
+// if use name or password is empty will responses error msg
+// if use name or password is error also will responses error msg
+func (l *LoginAPI) Login(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	err := GetJSONBodyFromRequest(r, &user)
+	// login request is error
+	if err != nil {
+		OK(w, err)
+		return
+	}
+	// user name is empty
+	if len(user.UserName) == 0 {
+		err = errors.New("user name is empty")
+		Error(w, err)
+		return
+	}
+	// password is empty
+	if len(user.Password) == 0 {
+		err = errors.New("password is empty")
+		Error(w, err)
+		return
+	}
+	// user name is error
+	if l.user.UserName != user.UserName {
+		err = errors.New("user name is error")
+		Error(w, err)
+		return
+	}
+	// password is error
+	if l.user.Password != user.Password {
+		err = errors.New("password is error")
+		Error(w, err)
+		return
+	}
+	token, err := middleware.CreateToken(user)
+	if err != nil {
+		Error(w, err)
+	}
+	OK(w, token)
+}
+
+// Check responses use msg
+// this method use for test
+func (l *LoginAPI) Check(w http.ResponseWriter, r *http.Request) {
+	OK(w, l.user)
+}
---
+++ broker/api/login_test.go
@@ -0,0 +1,107 @@
+package api
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/magiconair/properties/assert"
+	"gopkg.in/check.v1"
+
+	"github.com/eleme/lindb/broker/middleware"
+	"github.com/eleme/lindb/mock"
+	"github.com/eleme/lindb/models"
+)
+
+type testLoginAPISuite struct {
+	mock.RepoTestSuite
+}
+
+var test *testing.T
+
+func TestLoginApi(t *testing.T) {
+	check.Suite(&testLoginAPISuite{})
+	test = t
+	check.TestingT(t)
+}
+
+func (tl *testLoginAPISuite) TestLogin(c *check.C) {
+	user := models.User{UserName: "admin", Password: "admin123"}
+	api := NewLoginAPI(user)
+
+	//create success
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    user,
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 200,
+	})
+
+	//user failure error password
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    models.User{UserName: "admin", Password: "admin1234"},
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 500,
+	})
+
+	//user failure error user name
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    models.User{UserName: "123", Password: "admin123"},
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 500,
+	})
+
+	//user failure error password
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    models.User{UserName: "admin", Password: "admin1234"},
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 500,
+	})
+
+	//user login failure  password is empty
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    models.User{UserName: "admin"},
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 500,
+	})
+
+	//user login failure  user name is empty
+	mock.DoRequest(test, &mock.HTTPHandler{
+		Method:         http.MethodPost,
+		URL:            "/user",
+		RequestBody:    models.User{Password: "admin1234"},
+		HandlerFunc:    api.Login,
+		ExpectHTTPCode: 500,
+	})
+}
+
+func Test_JWT(t *testing.T) {
+	user := models.User{UserName: "admin", Password: "admin123"}
+	claims := middleware.CustomClaims{
+		UserName: user.UserName,
+		Password: user.Password,
+	}
+	cid := middleware.Md5Encrypt(user)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+	tokenString, _ := token.SignedString([]byte(cid))
+
+	assert.Equal(t,
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvc"+
+			"mQiOiJhZG1pbjEyMyJ9.YbNGN0V-U5Y3xOIGNXcgbQkK2VV30UDDEZV19FN62hk", tokenString)
+
+	mapClaims := middleware.CustomClaims{}
+	_, _ = jwt.ParseWithClaims(tokenString, &mapClaims, func(token *jwt.Token) (i interface{}, e error) {
+		return cid, nil
+	})
+	assert.Equal(t, user.Password, mapClaims.Password)
+	assert.Equal(t, user.UserName, mapClaims.UserName)
+}
