cbdba687910d3a07ea15c38c7ec84ad498c5c0dd
stone1100 <stonejh1100@gmail.com>
2019-08-21 15:35:19 +0800

    [feat:#179][sql]:encoding query statement

--- sql/stmt/expr_test.go
+++ sql/stmt/expr_test.go
@@ -52,3 +52,134 @@ func TestTagFilter(t *testing.T) {
 	assert.Equal(t, "tagKey", (&InExpr{Key: "tagKey", Values: []string{"a", "b", "c"}}).TagKey())
 	assert.Equal(t, "tagKey", (&RegexExpr{Key: "tagKey", Regexp: "Regexp"}).TagKey())
 }
+
+func TestExpr_Marshal_Fail(t *testing.T) {
+	data := Marshal(nil)
+	assert.Nil(t, data)
+}
+
+func TestExpr_Unmarshal_Fail(t *testing.T) {
+	_, err := Unmarshal([]byte{1, 2, 3})
+	assert.NotNil(t, err)
+	_, err = Unmarshal([]byte("{\"type\":\"unknown\"}"))
+	assert.NotNil(t, err)
+	_, err = unmarshal(&exprData{Type: "test", Expr: []byte{1, 2, 3}}, &EqualsExpr{})
+	assert.NotNil(t, err)
+	_, err = unmarshalCall([]byte{1, 2, 3})
+	assert.NotNil(t, err)
+	_, err = unmarshalCall([]byte("{\"type\":\"call\",\"params\":[\"213\"]}"))
+	assert.NotNil(t, err)
+	_, err = Unmarshal([]byte("{\"type\":\"paren\",\"expr\":[\"213\"]}"))
+	assert.NotNil(t, err)
+	_, err = Unmarshal([]byte("{\"type\":\"not\",\"expr\":[\"213\"]}"))
+	assert.NotNil(t, err)
+	_, err = unmarshalSelectItem([]byte("324"))
+	assert.NotNil(t, err)
+	_, err = unmarshalSelectItem([]byte("{\"type\":\"selectItem\",\"expr\":[\"213\"]}"))
+	assert.NotNil(t, err)
+	_, err = unmarshalBinary([]byte("123"))
+	assert.NotNil(t, err)
+	_, err = unmarshalBinary([]byte("{\"type\":\"binary\",\"left\":\"123\"}"))
+	assert.NotNil(t, err)
+	_, err = unmarshalBinary([]byte("{\"type\":\"binary\",\"left\":{\"type\":\"field\",\"expr\":{\"name\":\"f\"}}," +
+		"\"right\":\"123\"}"))
+	assert.NotNil(t, err)
+}
+
+func TestRegexExpr_Marshal(t *testing.T) {
+	expr := &RegexExpr{Key: "tagKey", Regexp: "Regexp"}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*RegexExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestLikeExpr_Marshal(t *testing.T) {
+	expr := &LikeExpr{Key: "tagKey", Value: "tagValue"}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*LikeExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestInExpr_Marshal(t *testing.T) {
+	expr := &InExpr{Key: "tagKey"}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*InExpr)
+	assert.Equal(t, *expr, *e)
+
+	expr = &InExpr{Key: "tagKey", Values: []string{"a", "b", "c"}}
+	data = Marshal(expr)
+	exprData, _ = Unmarshal(data)
+	e = exprData.(*InExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestEqualsExpr_Marshal(t *testing.T) {
+	expr := &EqualsExpr{Key: "tagKey", Value: "tagValue"}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*EqualsExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestNotExpr_Marshal(t *testing.T) {
+	expr := &NotExpr{
+		Expr: &EqualsExpr{Key: "tagKey", Value: "tagValue"},
+	}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*NotExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestSelectItem_Marshal(t *testing.T) {
+	expr := &SelectItem{Expr: &FieldExpr{Name: "f"}, Alias: "f1"}
+	data := Marshal(expr)
+	exprData, err := Unmarshal(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := exprData.(*SelectItem)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestCallExpr_Marshal(t *testing.T) {
+	expr := &CallExpr{FuncType: function.Sum, Params: []Expr{&FieldExpr{Name: "f"}}}
+	data := Marshal(expr)
+	exprData, err := Unmarshal(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := exprData.(*CallExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestParenExpr_Marshal(t *testing.T) {
+	expr := &ParenExpr{
+		Expr: &BinaryExpr{
+			Left:     &CallExpr{FuncType: function.Sum, Params: []Expr{&FieldExpr{Name: "f"}}},
+			Operator: ADD,
+			Right:    &FieldExpr{Name: "a"},
+		}}
+	data := Marshal(expr)
+	exprData, err := Unmarshal(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := exprData.(*ParenExpr)
+	assert.Equal(t, *expr, *e)
+}
+
+func TestBinaryExpr_Marshal(t *testing.T) {
+	expr := &BinaryExpr{
+		Left:     &CallExpr{FuncType: function.Sum, Params: []Expr{&FieldExpr{Name: "f"}}},
+		Operator: ADD,
+		Right:    &FieldExpr{Name: "a"},
+	}
+	data := Marshal(expr)
+	exprData, _ := Unmarshal(data)
+	e := exprData.(*BinaryExpr)
+	assert.Equal(t, *expr, *e)
+}
---
+++ sql/stmt/query_test.go
@@ -0,0 +1,73 @@
+package stmt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lindb/lindb/aggregation/function"
+	"github.com/lindb/lindb/pkg/encoding"
+	"github.com/lindb/lindb/pkg/timeutil"
+)
+
+func TestQuery_Marshal(t *testing.T) {
+	query := Query{
+		MetricName: "test",
+		SelectItems: []Expr{
+			&SelectItem{Expr: &FieldExpr{Name: "a"}},
+			&SelectItem{Expr: &FieldExpr{Name: "b"}},
+			&SelectItem{
+				Expr: &CallExpr{
+					FuncType: function.Stddev,
+					Params: []Expr{
+						&CallExpr{
+							FuncType: function.Max,
+							Params: []Expr{&CallExpr{
+								FuncType: function.Sum,
+								Params:   []Expr{&FieldExpr{Name: "c"}}},
+							},
+						},
+					},
+				},
+			},
+		},
+		Condition: &BinaryExpr{
+			Left: &ParenExpr{Expr: &BinaryExpr{
+				Left:     &InExpr{Key: "ip", Values: []string{"1.1.1.1", "2.2.2.2"}},
+				Operator: AND,
+				Right:    &EqualsExpr{Key: "region", Value: "sh"},
+			}},
+			Operator: AND,
+			Right: &ParenExpr{Expr: &BinaryExpr{
+				Left:     &EqualsExpr{Key: "path", Value: "/data"},
+				Operator: OR,
+				Right:    &EqualsExpr{Key: "path", Value: "/home"},
+			}},
+		},
+		TimeRange:    timeutil.TimeRange{Start: 10, End: 30},
+		Interval:     1000,
+		IntervalType: "10s",
+		GroupBy:      []string{"a", "b", "c"},
+		Limit:        100,
+	}
+
+	data := encoding.JSONMarshal(&query)
+	fmt.Println(string(data))
+	query1 := Query{}
+	err := encoding.JSONUnmarshal(data, &query1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, query, query1)
+}
+
+func TestQuery_Marshal_Fail(t *testing.T) {
+	query := &Query{}
+	err := query.UnmarshalJSON([]byte{1, 2, 3})
+	assert.NotNil(t, err)
+	err = query.UnmarshalJSON([]byte("{\"condition\":\"123\"}"))
+	assert.NotNil(t, err)
+	err = query.UnmarshalJSON([]byte("{\"selectItems\":[\"123\"]}"))
+	assert.NotNil(t, err)
+}
