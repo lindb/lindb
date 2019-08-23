567b77a82e0ba85f4155f9232c35a4ce30662e7f
codingcrush <codingcrush@163.com>
2019-08-23 12:27:45 +0800

    [feat][pkg:bufpool]: add buffer pool for sync.Buffer

---
+++ pkg/bufpool/pool_test.go
@@ -0,0 +1,22 @@
+package bufpool
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BufferPool(t *testing.T) {
+	buf1 := GetBuffer()
+	assert.Equal(t, 0, buf1.Len())
+
+	buf1.WriteByte(byte(66))
+	assert.Equal(t, 1, buf1.Len())
+	assert.Equal(t, 64, buf1.Cap())
+
+	PutBuffer(buf1)
+
+	buf2 := GetBuffer()
+	assert.Equal(t, 0, buf2.Len())
+	assert.Equal(t, 64, buf2.Cap())
+}
