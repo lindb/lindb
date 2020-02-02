1151be09185a2246391b460f3e91a1fef7a675f2
stone1100 <stonejh1100@gmail.com>
2020-02-02 17:06:41 +0800

    [feat:#307][metadb]: impl metadata database using bbolt

---
+++ tsdb/metadb/metedata_database.go
@@ -0,0 +1,370 @@
+package metadb
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/lindb/lindb/pkg/logger"
+	"github.com/lindb/lindb/pkg/timeutil"
+	"github.com/lindb/lindb/series"
+	"github.com/lindb/lindb/series/field"
+	"github.com/lindb/lindb/series/tag"
+)
+
+// for testing
+var (
+	createMetadataBackend = newMetadataBackend
+)
+
+var (
+	syncInterval = 2 * timeutil.OneSecond
+)
+
+// metadataDatabase implements the MetadataDatabase interface,
+// !!!!NOTICE: need cache all tag keys/fields of metric
+type metadataDatabase struct {
+	name    string // tsdb's name
+	ctx     context.Context
+	cancel  context.CancelFunc
+	backend MetadataBackend
+	metrics map[string]MetricMetadata // metadata cache(key: namespace + metric-name, value: metric metadata)
+
+	mutable      *metadataUpdateEvent // pending update events
+	immutable    *metadataUpdateEvent // syncing pending update events
+	lastSyncTime int64
+	syncSignal   chan struct{}
+
+	syncInterval int64
+
+	rwMux sync.RWMutex
+}
+
+// NewMetadataDatabase creates new metadata database
+func NewMetadataDatabase(ctx context.Context, name, parent string) (MetadataDatabase, error) {
+	backend, err := createMetadataBackend(parent)
+	if err != nil {
+		return nil, err
+	}
+	c, cancel := context.WithCancel(ctx)
+	mdb := &metadataDatabase{
+		name:         name,
+		ctx:          c,
+		cancel:       cancel,
+		backend:      backend,
+		mutable:      newMetadataUpdateEvent(),
+		metrics:      make(map[string]MetricMetadata),
+		syncSignal:   make(chan struct{}),
+		syncInterval: syncInterval,
+		lastSyncTime: timeutil.Now(),
+	}
+	go mdb.checkSync()
+	go mdb.syncPendingEvent()
+	return mdb, nil
+}
+
+// SuggestNamespace suggests the namespace by namespace's prefix
+func (mdb *metadataDatabase) SuggestNamespace(prefix string, limit int) (namespaces []string, err error) {
+	return mdb.backend.suggestNamespace(prefix, limit)
+}
+
+// SuggestMetricName suggests the metric name by name's prefix
+func (mdb *metadataDatabase) SuggestMetricName(namespace, prefix string, limit int) (namespaces []string, err error) {
+	return mdb.backend.suggestMetricName(namespace, prefix, limit)
+}
+
+// GetMetricID gets the metric id by namespace and metric name, if not exist return series.ErrNotFound
+func (mdb *metadataDatabase) GetMetricID(namespace, metricName string) (metricID uint32, err error) {
+	mdb.rwMux.RLock()
+	// read from memory
+	key := namespace + metricName
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		defer mdb.rwMux.RUnlock()
+		return metricMetadata.getMetricID(), nil
+	}
+	mdb.rwMux.RUnlock()
+
+	// read from meta db
+	return mdb.backend.getMetricID(namespace, metricName)
+}
+
+// GetTagKeyID gets the tag key id by namespace/metric name/tag key key, if not exist return series.ErrNotFound
+func (mdb *metadataDatabase) GetTagKeyID(namespace, metricName string, tagKey string) (tagKeyID uint32, err error) {
+	key := namespace + metricName
+
+	mdb.rwMux.RLock()
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		defer mdb.rwMux.RUnlock()
+		tagKeyID, ok = metricMetadata.getTagKeyID(tagKey)
+		if ok {
+			return
+		}
+		return 0, series.ErrNotFound
+	}
+	mdb.rwMux.RUnlock()
+
+	metricID, err := mdb.backend.getMetricID(namespace, metricName)
+	if err != nil {
+		return 0, err
+	}
+
+	return mdb.backend.getTagKeyID(metricID, tagKey)
+}
+
+// GetAllTagKeys returns the all tag keys by namespace/metric name, if not exist return series.ErrNotFound
+func (mdb *metadataDatabase) GetAllTagKeys(namespace, metricName string) (tags []tag.Meta, err error) {
+	key := namespace + metricName
+	mdb.rwMux.RLock()
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		defer mdb.rwMux.RUnlock()
+		return metricMetadata.getAllTagKeys(), nil
+	}
+	mdb.rwMux.RUnlock()
+
+	metricID, err := mdb.backend.getMetricID(namespace, metricName)
+	if err != nil {
+		return
+	}
+
+	return mdb.backend.getAllTagKeys(metricID)
+}
+
+// GetField gets the field meta by namespace/metric name/field name, if not exist return series.ErrNotFound
+func (mdb *metadataDatabase) GetField(namespace, metricName, fieldName string) (f field.Meta, err error) {
+	key := namespace + metricName
+	mdb.rwMux.RLock()
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		defer mdb.rwMux.RUnlock()
+		f, ok = metricMetadata.getField(fieldName)
+		if ok {
+			return f, nil
+		}
+		return field.Meta{}, series.ErrNotFound
+	}
+	mdb.rwMux.RUnlock()
+	metricID, err := mdb.GetMetricID(namespace, metricName)
+	if err != nil {
+		return field.Meta{}, err
+	}
+
+	// read from db
+	return mdb.backend.getField(metricID, fieldName)
+}
+
+func (mdb *metadataDatabase) GetAllFields(namespace, metricName string) (fields []field.Meta, err error) {
+	key := namespace + metricName
+	mdb.rwMux.RLock()
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		defer mdb.rwMux.RUnlock()
+		return metricMetadata.getAllFields(), nil
+	}
+	mdb.rwMux.RUnlock()
+	metricID, err := mdb.GetMetricID(namespace, metricName)
+	if err != nil {
+		return nil, err
+	}
+	return mdb.backend.getAllFields(metricID)
+}
+
+// GenMetricID generates the metric id in the memory.
+// 1) get metric id from memory if exist, if not exist goto 2
+// 2) get metric metadata from backend storage, if not exist need create new metric metadata
+func (mdb *metadataDatabase) GenMetricID(namespace, metricName string) (metricID uint32, err error) {
+	key := namespace + metricName
+	mdb.rwMux.RLock()
+	// get metric id from memory, add read lock
+	metricMetadata, ok := mdb.metrics[key]
+	if ok {
+		mdb.rwMux.RUnlock()
+		return metricMetadata.getMetricID(), nil
+	}
+	mdb.rwMux.RUnlock()
+
+	// assign metric id from memory, add write lock
+	mdb.rwMux.Lock()
+	defer mdb.rwMux.Unlock()
+	// double check with memory
+	metricMetadata, ok = mdb.metrics[key]
+	if ok {
+		return metricMetadata.getMetricID(), nil
+	}
+
+	// load metric metadata from backend storage
+	metricMetadata, err = mdb.backend.loadMetricMetadata(namespace, metricName)
+	if err == nil {
+		// get metric metadata from backend
+		mdb.metrics[key] = metricMetadata
+		return metricMetadata.getMetricID(), nil
+	}
+	// isn't not found, return err
+	if err != series.ErrNotFound {
+		return
+	}
+	// assign new metric id
+	metricID = mdb.backend.genMetricID()
+	mdb.metrics[key] = newMetricMetadata(metricID, 0)
+	mdb.mutable.addMetric(namespace, metricName, metricID)
+
+	mdb.notifySyncWithoutLock(false)
+	return metricID, nil
+}
+
+// GenFieldID generates the field id in the memory,
+// !!!!! NOTICE: metric metadata must be exist in memory, because gen metric has been saved
+func (mdb *metadataDatabase) GenFieldID(namespace, metricName string,
+	fieldName string, fieldType field.Type,
+) (fieldID uint16, err error) {
+	key := namespace + metricName
+
+	mdb.rwMux.Lock()
+	defer mdb.rwMux.Unlock()
+	// read from memory metric metadata
+	metricMetadata := mdb.metrics[key]
+	f, ok := metricMetadata.getField(fieldName)
+	if ok {
+		if f.Type == fieldType {
+			return f.ID, nil
+		}
+		return 0, series.ErrWrongFieldType
+	}
+	// assign new field id
+	fieldID, err = metricMetadata.createField(fieldName, fieldType)
+	if err != nil {
+		return 0, err
+	}
+	mdb.mutable.addField(metricMetadata.getMetricID(), field.Meta{
+		ID:   fieldID,
+		Type: fieldType,
+		Name: fieldName,
+	})
+	mdb.notifySyncWithoutLock(false)
+	return
+}
+
+// GenTagKeyID generates the tag key id in the memory
+// !!!!! NOTICE: metric metadata must be exist in memory, because gen metric has been saved
+func (mdb *metadataDatabase) GenTagKeyID(namespace, metricName, tagKey string) (tagKeyID uint32, err error) {
+	key := namespace + metricName
+
+	mdb.rwMux.Lock()
+	defer mdb.rwMux.Unlock()
+	// read from memory metric metadata
+	metricMetadata := mdb.metrics[key]
+	tagKeyID, ok := metricMetadata.getTagKeyID(tagKey)
+	if ok {
+		return tagKeyID, nil
+	}
+	// check tag keys count before create
+	if err = metricMetadata.checkTagKeyCount(); err != nil {
+		return 0, err
+	}
+	// assign new tag key id
+	tagKeyID = mdb.backend.genTagKeyID()
+	metricMetadata.createTagKey(tagKey, tagKeyID)
+	mdb.mutable.addTagKey(metricMetadata.getMetricID(), tag.Meta{
+		Key: tagKey,
+		ID:  tagKeyID,
+	})
+	mdb.notifySyncWithoutLock(false)
+	return
+}
+
+// Sync syncs the bbolt.DB's data file
+func (mdb *metadataDatabase) Sync() error {
+	//FIXME stone100 need impl sync force when flush metric data
+	return mdb.backend.sync()
+}
+
+// Close closes the resources
+func (mdb *metadataDatabase) Close() error {
+	mdb.cancel()
+	mdb.rwMux.Lock()
+	defer mdb.rwMux.Unlock()
+
+	if mdb.mutable != nil && !mdb.mutable.isEmpty() {
+		if err := mdb.backend.saveMetadata(mdb.mutable); err != nil {
+			return err
+		}
+	}
+	if mdb.immutable != nil && !mdb.mutable.isEmpty() {
+		if err := mdb.backend.saveMetadata(mdb.immutable); err != nil {
+			return err
+		}
+	}
+	return mdb.backend.Close()
+}
+
+// checkSync checks if need sync pending metadata event in period
+func (mdb *metadataDatabase) checkSync() {
+	ticker := time.NewTicker(time.Duration(mdb.syncInterval * 1000000))
+	for {
+		select {
+		case <-ticker.C:
+			mdb.notifySyncWithLock(false)
+		case <-mdb.ctx.Done():
+			ticker.Stop()
+			metaLogger.Info("check event update goroutine exit...", logger.String("db", mdb.name))
+			return
+		}
+	}
+}
+
+// notifySyncWithoutLock notifies sync goroutine need save pending metadata events with lock
+func (mdb *metadataDatabase) notifySyncWithLock(force bool) {
+	mdb.rwMux.Lock()
+	defer mdb.rwMux.Unlock()
+
+	mdb.notifySyncWithoutLock(force)
+}
+
+// notifySyncWithoutLock notifies sync goroutine need save pending metadata events without lock
+func (mdb *metadataDatabase) notifySyncWithoutLock(force bool) {
+	if (!mdb.mutable.isFull() || !force) && timeutil.Now()-mdb.lastSyncTime < mdb.syncInterval {
+		return
+	}
+
+	if !mdb.mutable.isEmpty() && mdb.immutable == nil {
+		mdb.immutable = mdb.mutable
+		mdb.mutable = newMetadataUpdateEvent()
+		// notify with time out
+		select {
+		case mdb.syncSignal <- struct{}{}:
+		case <-time.After(time.Second):
+			//FIXME add metric
+			metaLogger.Error("notify sync metadata save timeout", logger.String("db", mdb.name))
+		}
+	}
+}
+
+// syncPendingEvent syncs the pending metadata event
+func (mdb *metadataDatabase) syncPendingEvent() {
+	for {
+		select {
+		case <-mdb.syncSignal:
+			var event *metadataUpdateEvent
+			mdb.rwMux.RLock()
+			event = mdb.immutable
+			mdb.rwMux.RUnlock()
+			if event == nil {
+				continue
+			}
+			if err := mdb.backend.saveMetadata(event); err != nil {
+				//FIXME stone1100 add metric
+				metaLogger.Error("save metadata err", logger.String("db", mdb.name), logger.Error(err))
+				continue
+			}
+			mdb.rwMux.Lock()
+			mdb.immutable = nil
+			mdb.lastSyncTime = timeutil.Now()
+			mdb.rwMux.Unlock()
+		case <-mdb.ctx.Done():
+			metaLogger.Info("sync update event goroutine exit...", logger.String("db", mdb.name))
+			return
+		}
+	}
+}
---
+++ tsdb/metadb/metric_metadata.go
@@ -0,0 +1,122 @@
+package metadb
+
+import (
+	"go.uber.org/atomic"
+
+	"github.com/lindb/lindb/constants"
+	"github.com/lindb/lindb/series"
+	"github.com/lindb/lindb/series/field"
+	"github.com/lindb/lindb/series/tag"
+)
+
+//go:generate mockgen -source ./metric_metadata.go -destination=./metric_metadata_mock.go -package=metadb
+
+// MetricMetadata represents the metric metadata for memory cache,
+// it will cache all tag keys and fields in backend db
+type MetricMetadata interface {
+	// initialize initializes the metric metadata with tags/fields
+	initialize(fields []field.Meta, tagKeys []tag.Meta)
+
+	// getMetricID gets the metric id
+	getMetricID() uint32
+	// getField gets the field meta by field name, if not exist return false
+	getField(fieldName string) (field.Meta, bool)
+	// getAllFields returns the all fields of the metric
+	getAllFields() (fields []field.Meta)
+	// getTagKeyID gets the tag key id by tag key, if not exist return false
+	getTagKeyID(tagKey string) (uint32, bool)
+	// getAllTags returns the tag keys of the metric
+	getAllTagKeys() (tagKeys []tag.Meta)
+
+	// checkTagKeyCount checks the tag keys if limit, if limit return series.ErrTooManyTagKeys
+	checkTagKeyCount() error
+
+	// createField creates the field meta, if success return field id, else return series.ErrTooManyFields
+	createField(fieldName string, fieldType field.Type) (uint16, error)
+	// createTagKey creates the tag key
+	createTagKey(tagKey string, tagKeyID uint32)
+}
+
+// metricMetadata implements MetricMetadata interface
+type metricMetadata struct {
+	metricID   uint32
+	fieldIDSeq atomic.Int32
+	fields     []field.Meta
+	tagKeys    []tag.Meta
+}
+
+// newMetricMetadata creates the metric metadata with metric id and field id assign sequence
+func newMetricMetadata(metricID uint32, fieldIDSeq int32) MetricMetadata {
+	mm := &metricMetadata{
+		metricID: metricID,
+	}
+	mm.fieldIDSeq.Store(fieldIDSeq)
+	return mm
+}
+
+// initialize initializes the metric metadata with tags/fields
+func (mm *metricMetadata) initialize(fields []field.Meta, tagKeys []tag.Meta) {
+	mm.fields = fields
+	mm.tagKeys = tagKeys
+}
+
+// getMetricID gets the metric id
+func (mm *metricMetadata) getMetricID() uint32 {
+	return mm.metricID
+}
+
+// getField gets the field meta by field name, if not exist return false
+func (mm *metricMetadata) getField(fieldName string) (field.Meta, bool) {
+	for _, f := range mm.fields {
+		if f.Name == fieldName {
+			return f, true
+		}
+	}
+	return field.Meta{}, false
+}
+
+// getAllFields returns the all fields of the metric
+func (mm *metricMetadata) getAllFields() (fields []field.Meta) {
+	return mm.fields
+}
+
+// getTagKeyID gets the tag key id by tag key, if not exist return false
+func (mm *metricMetadata) getTagKeyID(tagKey string) (uint32, bool) {
+	for _, t := range mm.tagKeys {
+		if t.Key == tagKey {
+			return t.ID, true
+		}
+	}
+	return 0, false
+}
+
+// getAllTags returns the tag keys of the metric
+func (mm *metricMetadata) getAllTagKeys() (tagKeys []tag.Meta) {
+	return mm.tagKeys
+}
+
+// createField creates the field meta, if success return field id, else return series.ErrTooManyFields
+func (mm *metricMetadata) createField(fieldName string, fieldType field.Type) (uint16, error) {
+	// check fields count
+	if mm.fieldIDSeq.Load() >= constants.DefaultMaxFieldsCount {
+		return 0, series.ErrTooManyFields
+	}
+	// create new field
+	fieldID := uint16(mm.fieldIDSeq.Inc())
+	mm.fields = append(mm.fields, field.Meta{ID: fieldID, Name: fieldName, Type: fieldType})
+	return fieldID, nil
+}
+
+// checkTagKeyCount checks the tag keys if limit, if limit return series.ErrTooManyTagKeys
+func (mm *metricMetadata) checkTagKeyCount() error {
+	// check tag keys count
+	if len(mm.tagKeys) >= constants.DefaultMaxTagKeysCount {
+		return series.ErrTooManyTagKeys
+	}
+	return nil
+}
+
+// createTagKey creates the tag key
+func (mm *metricMetadata) createTagKey(tagKey string, tagKeyID uint32) {
+	mm.tagKeys = append(mm.tagKeys, tag.Meta{ID: tagKeyID, Key: tagKey})
+}
