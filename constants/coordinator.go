05d03fc06a8283ad56193e6ccc07dfd93dcaae50
stone1100 <stonejh1100@gmail.com>
2021-09-02 13:44:33 +0800

    [refactor]: create shard logic in storage cluster

--- constants/coordinator.go
+++ constants/coordinator.go
@@ -48,8 +48,6 @@ const (
 
 // defines all task kinds
 const (
-	// CreateShard represents task kind which is create shard for storage node
-	CreateShard task.Kind = "create-shard"
 	// FlushDatabase represents task kind which is flush memory database for storage node
 	FlushDatabase task.Kind = "flush-database"
 )
