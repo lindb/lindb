743ae959df327aab35c1173c8647888fbf78212d
stone1100 <stonejh1100@gmail.com>
2021-09-08 22:50:14 +0800

    [feat]: move acceptable time range check from storage to broker

--- tsdb/shard_test.go
+++ tsdb/shard_test.go
@@ -167,7 +167,7 @@ func TestShard_GetDataFamilies(t *testing.T) {
 }
 
 func Test_Shard_validateMetric(t *testing.T) {
-	s := &shard{behind: 100000, ahead: 100000, metrics: *newShardMetrics("1", 1)}
+	s := &shard{metrics: *newShardMetrics("1", 1)}
 	assert.Zero(t, s.CurrentInterval().Int64())
 	// nil pb
 	err := s.validateMetric(nil)
@@ -177,15 +177,6 @@ func Test_Shard_validateMetric(t *testing.T) {
 	assert.Error(t, err)
 	// field empty
 	err = s.validateMetric(&protoMetricsV1.Metric{Name: "1"})
-	assert.Error(t, err)
-
-	// time behind
-	err = s.validateMetric(&protoMetricsV1.Metric{
-		Name: "1",
-		SimpleFields: []*protoMetricsV1.SimpleField{
-			{Name: "1", Type: protoMetricsV1.SimpleFieldType_DELTA_SUM, Value: 1},
-		},
-	})
 	assert.Error(t, err)
 
 	// bad tags, empty
@@ -390,27 +381,7 @@ func TestShard_Write(t *testing.T) {
 		Name:      "test",
 		Timestamp: timestamp,
 	}))
-	// case 4: reject before
-	assert.Error(t, shardINTF.Write(&protoMetricsV1.Metric{
-		Name:      "test",
-		Timestamp: timestamp - 2*timeutil.OneMinute,
-		SimpleFields: []*protoMetricsV1.SimpleField{{
-			Name:  "f1",
-			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
-			Value: 1.0,
-		}},
-	}))
-	// case 5: reject ahead
-	assert.Error(t, shardINTF.Write(&protoMetricsV1.Metric{
-		Name:      "test",
-		Timestamp: timestamp + 2*timeutil.OneMinute,
-		SimpleFields: []*protoMetricsV1.SimpleField{{
-			Name:  "f1",
-			Value: 1.0,
-			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
-		}},
-	}))
-	// case 6: gen metric id err
+	// case 4: gen metric id err
 	metadataDB.EXPECT().GenMetricID(constants.DefaultNamespace, "test").Return(uint32(0), fmt.Errorf("err"))
 	assert.Error(t, shardINTF.Write(&protoMetricsV1.Metric{
 		Name:      "test",
@@ -421,7 +392,7 @@ func TestShard_Write(t *testing.T) {
 			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
 		}},
 	}))
-	// case 7: gen series id err
+	// case 5: gen series id err
 	metadataDB.EXPECT().GenMetricID(constants.DefaultNamespace, "test").Return(uint32(10), nil).AnyTimes()
 	indexDB.EXPECT().GetOrCreateSeriesID(uint32(10), uint64(9)).Return(uint32(0), false, fmt.Errorf("err"))
 	assert.Error(t, shardINTF.Write(&protoMetricsV1.Metric{
@@ -435,7 +406,7 @@ func TestShard_Write(t *testing.T) {
 			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
 		}},
 	}))
-	// case 8: get old series id
+	// case 6: get old series id
 	metadataDB.EXPECT().GenMetricID(constants.DefaultNamespace, "test").Return(uint32(10), nil).AnyTimes()
 	metadataDB.EXPECT().GenFieldID(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(field.ID(1), nil)
 	indexDB.EXPECT().GetOrCreateSeriesID(uint32(10), uint64(11)).Return(uint32(10), false, nil)
@@ -450,7 +421,7 @@ func TestShard_Write(t *testing.T) {
 			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
 		}},
 	}))
-	// case 9: create new series id
+	// case 7: create new series id
 	indexDB.EXPECT().GetOrCreateSeriesID(uint32(10), uint64(10)).Return(uint32(10), true, nil)
 	metadataDB.EXPECT().GenFieldID(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(field.ID(1), nil)
 	indexDB.EXPECT().BuildInvertIndex(constants.DefaultNamespace, "test", tag.KeyValuesFromMap(map[string]string{"ip": "1.1.1.1"}), uint32(10))
@@ -465,7 +436,7 @@ func TestShard_Write(t *testing.T) {
 			Type:  protoMetricsV1.SimpleFieldType_DELTA_SUM,
 		}},
 	}))
-	// case 10: write metric without tags
+	// case 8: write metric without tags
 	metadataDB.EXPECT().GenFieldID(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(field.ID(1), nil)
 	assert.NoError(t, shardINTF.Write(&protoMetricsV1.Metric{
 		Name:      "test",
