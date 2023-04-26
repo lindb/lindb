78a405ba7aaaf6c889df2fe68c1eb2135659c86d
stone1100 <stonejh1100@gmail.com>
2023-04-26 23:05:57 +0800

    [chore]: add style lint (#977)
    Signed-off-by: stone1100 <stonejh1100@gmail.com>

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -6,7 +6,6 @@ ownership. LinDB licenses this file to you under
 the Apache License, Version 2.0 (the "License"); you may
 not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
  
 Unless required by applicable law or agreed to in writing,
@@ -48,29 +47,6 @@ module.exports = {
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/consistent-type-assertions": "off",
     "@typescript-eslint/no-use-before-define": "error",
-    "header/header": [
-      2,
-      "block",
-      [
-        `
-Licensed to LinDB under one or more contributor
-license agreements. See the NOTICE file distributed with
-this work for additional information regarding copyright
-ownership. LinDB licenses this file to you under
-the Apache License, Version 2.0 (the "License"); you may
-not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
- 
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-`,
-      ],
-    ],
+    "header/header": [2, "header.js"],
   },
 };
